781d17921e6f2b72ca49a125cc36851afabbc338
Ismael Fuentes <ismafuentes@hotmail.es>
2022-11-03 09:39:28 +0100

    font awesome icons + styling

--- src/renderer.js
+++ src/renderer.js
@@ -6,7 +6,6 @@ document.querySelector('#add-url-btn').addEventListener('click', (event) => {
   const url = selector.value?.trim();
 
   if (state.videos.map((item) => item.itemUrl).includes(url)) {
-    alert('Duplicated url.');
     return;
   }
 
@@ -22,53 +21,40 @@ document.querySelector('#add-url-btn').addEventListener('click', (event) => {
           <span>${videoDetails.ownerChannelName} - ${videoDetails.category}</span>
           <p>${videoDetails.title}</p>
       </div>
-      <button class="yt-item-remove-btn">remove</button>
+      <button class="yt-item-remove-btn" id="btn-remove-${item.index}">
+        <span class="material-icons-outlined">delete</span>
+      </button>
     </div>
     `;
-
     document.querySelector('#items').appendChild(element);
+    registerDeleteAction(item);
+    selector.value = '';
   });
 });
+
+const registerDeleteAction = (item) => {
+  document.querySelector(`#btn-remove-${item.index}`).addEventListener('click', (e) => {
+    document.querySelector(`#item-${item.index}`).remove();
+    state.videos.splice(item.index, 1);
+  });
+};
 
 document.querySelector('#download-btn').addEventListener('click', (event) => {
   APP_CONTEXT.openDialog().then((openDialogResult) => {
     const { canceled, filePaths } = openDialogResult;
     if (!canceled) {
       for (const item of state.videos) {
-        APP_CONTEXT.downloadFromInfo(item, `${filePaths[0]}\\${item.videoDetails.title}.mp3`, {
+        const title = formatVideoTitle(item.videoDetails.title);
+        APP_CONTEXT.downloadFromInfo(item, `${filePaths[0]}\\${title}.mp3`, {
           quality: 'highestaudio',
         }).then(() => {
-          document.querySelector(`#item-${item.index}`).style.background = '#72B951';
+          console.log('download completed.');
         });
       }
     }
   });
 });
 
-// const renderData = (parent, max) => {
-//   for (let i = 0; i < max; i++) {
-//     const el = document.createElement('div');
-//     el.innerHTML = renderVideoDetails({
-//       index: i,
-//       url: '',
-//       ownerChannelName: 'test',
-//       category: 'music',
-//       title: 'test',
-//     });
-//     parent.appendChild(el);
-//   }
-// };
-
-// const renderVideoDetails = (videoDetails) => {
-//   return `
-//     <div id="item-${videoDetails.index}" class="yt-item">
-//         <img src="${videoDetails.url}" class="yt-item-thumb">
-//         <div class="yt-item-description">
-//             <span>${videoDetails.ownerChannelName} - ${videoDetails.category}</span>
-//             <p>${videoDetails.title}</p>
-//         </div>
-//     </div>
-//     `;
-// };
-
-// renderData(document.querySelector('#items'), 15);
+const formatVideoTitle = (title) => {
+  return title.replace(/(\||\,\s)/gi, '-');
+};
