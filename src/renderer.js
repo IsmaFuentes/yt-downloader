0e174100355395ca3e6db225fe85166c98f0368f
Ismael Fuentes <ismafuentes@hotmail.es>
2022-11-07 18:05:43 +0100

    toast notifications

--- src/renderer.js
+++ src/renderer.js
@@ -37,32 +37,50 @@ document.querySelector('#add-url-btn').addEventListener('click', (event) => {
 
 document.querySelector('#clear-cache-btn').addEventListener('click', (event) => {
   localStorage.clear();
+  showNotification('Download folder removed', 'warning', 2000);
 });
 
-document.querySelector('#download-btn').addEventListener('click', (event) => {
+document.querySelector('#download-btn').addEventListener('click', async (event) => {
   let download_path = localStorage.getItem('download-path');
   if (!download_path) {
-    APP_CONTEXT.openDialog({ title: 'Select a download folder' }).then((openDialogResult) => {
-      const { canceled, filePaths } = openDialogResult;
-      if (!canceled) {
-        download_path = filePaths[0];
-        localStorage.setItem('download-path', download_path);
-      }
-    });
+    const options = { title: 'Select a download folder' };
+    const { canceled, filePaths } = await APP_CONTEXT.openDialog(options);
+    if (!canceled) {
+      download_path = filePaths[0];
+      localStorage.setItem('download-path', download_path);
+    }
   }
 
-  downloadQueuedVideos(download_path);
+  await downloadQueuedVideos(download_path);
 });
 
-const downloadQueuedVideos = (donwloadPath) => {
+const downloadQueuedVideos = async (donwloadPath) => {
   for (const item of state.videos) {
     const title = item.videoDetails.title.replace(/(\||\,\s)/gi, '-');
     APP_CONTEXT.downloadFromInfo(item, `${donwloadPath}\\${title}.mp3`, {
       quality: 'highestaudio',
-    }).then(() => {
-      console.log('download completed.');
-      document.querySelector(`#item-${item.index}`).remove();
-      state.videos.splice(item.index, 1);
-    });
+    })
+      .then(() => {
+        document.querySelector(`#item-${item.index}`).remove();
+        state.videos.splice(item.index, 1);
+        showNotification('Download completed', 'success', 2000);
+      })
+      .catch((err) => {
+        if (err) {
+          showNotification(err.message, 'danger', 2000);
+        }
+      });
   }
+};
+
+const showNotification = (message, notificationType, delayMs) => {
+  const notificationWrapper = document.querySelector('#form-notification-wrapper');
+  const flyout = notificationWrapper.querySelectorAll('div')[0];
+  flyout.innerText = message;
+  flyout.className = `form-notification ${notificationType}`;
+  notificationWrapper.style.opacity = '1';
+  notificationWrapper.style.margin = '0 auto';
+  setTimeout(() => {
+    notificationWrapper.style.opacity = '0';
+  }, delayMs);
 };
